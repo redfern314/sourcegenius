f331b05de5e1a9de1baadd4a3db1c63c84f95ed8
YoMarco Salazar <salazarm@mit.edu>
2013-09-13 16:30:30 -0700

    added event to grab text and make new source document in collection

--- m.js
+++ m.js
@@ -1,15 +1,19 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to m.";
-  };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
+  Template.newSource.events({
+    'click #submit-new-source' : function(ev, page) {
+      var $textbox = page.find('textarea');
+      var source = $textbox.val();
+      Source.insert({ 'source' : source, shared: [], author: Meteor.userId }, function(error, result) {
+        if (error) {
+          $textbox.val('');
+          alert('An unknown error occurred');
+        } else {
+          
+        }
+      });
     }
-  });
+  })
 }
 
 if (Meteor.isServer) {
