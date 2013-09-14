f4acec792a18eeeec992ee3e2282bb1f75738122
YoMarco Salazar <salazarm@mit.edu>
2013-09-13 19:55:38 -0700

    added templates

--- m.js
+++ m.js
@@ -8,17 +8,38 @@ if (Meteor.isClient) {
 
   Template.newSource.events({
     'click #submit-new-source' : function(ev, page) {
-      var $textbox = page.find('textarea');
-      var source = $textbox.val();
-      Source.insert({ 'source' : source, shared: [], author: Meteor.userId }, function(error, result) {
+      var textbox = page.find('textarea');
+      var source = $(textbox).val();
+      Source.insert({ 'source' : source, shared: [], author: Meteor.userId() }, function(error, result) {
         if (error) {
           alert('An unknown error occurred');
         } else {
-          $textbox.val('');
+          console.log(result);
+          Session.set('viewing', result._id);
+          $(textbox).val('');
         }
       });
     }
-  })
+  });
+
+  Template.sources.viewing = function() {
+    return Session.get('viewing') ? true : false;
+  }
+
+  Template.sources.userSources = function() {
+    return Source.find({ author: Meteor.userId() }).fetch();
+  }
+
+  Template.sources.sharedSources = function() {
+    var sources = Source.find().fetch(),
+      resultsArray = [];
+    for (var i=0, l=sources.length; i<l; i++) {
+      if (_.contains(sources[i].shared, Meteor.userId() )) {
+        resultsArray.push(sources[i]);
+      }
+    }
+    return resultsArray;
+  }
 }
 
 if (Meteor.isServer) {
